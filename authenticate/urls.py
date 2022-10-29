a0c2880cdf4e56753d9f3564afd037da2400bf58
sowbhagya45 <69098233+sowbhagya45@users.noreply.github.com>
2022-10-29 14:07:08 +0530

    updation

--- authenticate/urls.py
+++ authenticate/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path("",views.signin,name="signin"),
     path("index",views.index,name="index"),
     path("pred",application.predict,name="pred"),
+    path("signout",views.signout,name="signout"),
 ]
--- authenticate/views.py
+++ authenticate/views.py
@@ -9,6 +9,13 @@ from django.contrib.auth.models import User
 from django.contrib import messages
 from django.contrib.auth import authenticate,login
 from django.core.files.storage import FileSystemStorage
+class Cred:
+    uname=""
+    fname=""
+    lname=""
+    pass1=""
+    tog=False
+obj=Cred()
 
 # Create your views here.
 def home(request):
@@ -16,55 +23,62 @@ def home(request):
 
 def signup(request):
     if request.method=="POST":
-        uname = request.POST['username']
-        fname = request.POST['fname']
-        lname = request.POST['lname']
+        obj.uname = request.POST['username']
+        obj.fname = request.POST['fname']
+        obj.lname = request.POST['lname']
         emai = request.POST['email']
-        pass1 = request.POST['pass1']
+        obj.pass1 = request.POST['pass1']
         pass2 = request.POST['pass2']
 
-        myuser = User.objects.create_user(uname, emai, pass1)
-        myuser.first_name = fname
-        myuser.last_name = lname
+        myuser = User.objects.create_user(obj.uname, emai, obj.pass1)
+        myuser.first_name = obj.fname
+        myuser.last_name = obj.lname
         myuser.save()
-        messages.success(request,"Account has been successfully created for "+uname)
+        messages.success(request,"Account has been successfully created for "+obj.uname)
         return redirect('signin')
     return render(request,"authenticate/signup.html")
 
 def signin(request):
     if request.method=="POST":
-        uname = request.POST['username']
-        pas = request.POST['pass']
-        user=authenticate(username=uname, password=pas)
+        obj.uname = request.POST['username']
+        obj.pass1 = request.POST['pass']
+        user=authenticate(username=obj.uname, password=obj.pass1)
         if user is not None:
+            obj.tog=True
             login(request,user)
             return redirect("index")
             #return render(request,"site/index.html",{"un":uname})
         else:
             messages.error(request,"Bad credentials!")
-            return redirect("signin") 
-
+            return redirect("signin")
     return render(request,"authenticate/signin.html")
 
-def index(request):
-    
-    try:
-        if request.method == 'POST':
-            f = request.FILES['bt_image']
-            filename = str(f.name)
-            if filename!='':
-                ext = filename.split(".")
-                if ext[1] in ['png', 'jpg', 'jpeg']:
-                    filename = secure_filename(f.name)
-                    fs = FileSystemStorage()
-                    fs.save(f.name,f)
-                    with open("authenticate/static/media/"+filename,'rb') as img:
-                        img1=img.name.split('/')[-1]
-                        x= application.predict(request,img)
-                        x['img'] = img1
-                    return render(request,"site/pred.html",x)
+def signout(request):
+    obj.tog=False 
+    return redirect("signin")
 
-    except Exception as e:
-        print("Exception\n")
-        print(e, '\n')
-    return render(request,'site/index.html')
+def index(request):
+    if obj.tog:
+        try:
+            if request.method == 'POST':
+                f = request.FILES['bt_image']
+                filename = str(f.name)
+                if filename!='':
+                    ext = filename.split(".")
+                    if ext[1] in ['png', 'jpg', 'jpeg']:
+                        filename = secure_filename(f.name)
+                        fs = FileSystemStorage()
+                        fs.save(f.name,f)
+                        with open("authenticate/static/media/"+filename,'rb') as img:
+                            img1=img.name.split('/')[-1]
+                            x= application.predict(request,img)
+                            x['img'] = img1
+                        return render(request,"site/pred.html",x)
+
+        except Exception as e:
+            print("Exception\n")
+            print(e, '\n')
+        
+        return render(request,'site/index.html',{"n":obj.uname})
+    else:
+        return redirect("signin")
